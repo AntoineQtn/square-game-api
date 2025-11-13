6ec681f27ded6522295663c84b8d92eae1d0157f
Antoine QUINTIN <antoine.quintin@le-campus-numerique.fr>
2025-11-13 10:01:07 +0100

    Initial commit

---
+++ src/test/java/org/example/squaregameapi/SquareGameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.squaregameapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareGameApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
